df8f67c052087b7caefef1e84c29e7a60b1c1a44
Christoph Sven Funke <csfunke@yahoo.com>
2026-04-13 11:15:39 +0200

    Add end-to-end tests

--- zen_europe/cli.py
+++ zen_europe/cli.py
@@ -1,7 +1,7 @@
 import argparse
 from pathlib import Path
 
-from zen_europe.elements.runner import run
+from zen_europe.model_creator import create_model
 
 
 def zen_europe_cli() -> None:
@@ -9,8 +9,28 @@ def zen_europe_cli() -> None:
     parser.add_argument(
         "--config",
         type=Path,
-        required=True,
+        required=False,
+        default=None,
         help="Path to the model configuration file.",
     )
+    parser.add_argument(
+        "--name",
+        type=str,
+        required=False,
+        default="zen-europe",
+        help="Name of the model that will be used when saving.",
+    )
+    parser.add_argument(
+        "--output_folder",
+        type=Path,
+        required=False,
+        default=".",
+        help="Output directory to which the model will be saved.",
+    )
     args = parser.parse_args()
-    run(args.config)
+
+    create_model(
+        config=args.config,
+        name=args.name,
+        output_folder=args.output_folder
+    )
--- zen_europe/model_creator.py
+++ zen_europe/model_creator.py
@@ -3,19 +3,29 @@ from pathlib import Path
 from zen_creator import Model
 
 
-def run(config: Path | str) -> None:
+def create_model(
+    config: Path | str | None = None,
+    name: str = "zen-europe",
+    output_folder: Path | str = ".",
+    write: bool = True,
+) -> Model:
 
     # Get path to crystal ball model
-    zen_europe_package_dir = Path(__file__).resolve().parent
-    crystal_ball_path = zen_europe_package_dir / "input" / "crystal_ball"
+    zen_europe_package_dir = Path(__file__).resolve().parent.parent
+    crystal_ball_path = zen_europe_package_dir / "data" / "crystal_ball"
 
     # load crystal ball model as starting point
     # TODO: this should be remove in the long run and replaced
     # with model.from_config()
     model = Model.from_existing(crystal_ball_path, config=config)
+    model.output_folder = Path(output_folder)
+    model.name = name
 
     # apply changes
     model.build()
 
     # save model output
-    model.write()
+    if write:
+        model.write()
+
+    return model
